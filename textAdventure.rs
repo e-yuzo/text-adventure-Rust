8c863416ac93fb59a2305d761413fa513526a105
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2018-03-31 22:36:39 -0300

    more functions to be implemented

--- textAdventure.rs
+++ textAdventure.rs
@@ -1,4 +1,9 @@
-//use std::env;
+#[allow(warnings)]
+//#![warn(unused_imports)]
+//#![warn(unused_variables)]
+
+use std::env;
+use std::io;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -8,12 +13,25 @@ struct Game {
 }
 
 impl Game {
-    fn set_actual_scene (&mut self, actual_scene: i32) {
+//pub fn new() -> Game{ cant initiate empty struct ---------------------
+    /*fn new() -> Game{
+        return Game {
+            scenes: {},
+            actual_scene: 0,
+        };
+    }*/
+    fn set_actual_scene(&mut self, actual_scene: i32) {
         self.actual_scene = actual_scene;
     }
-
-    fn get_actual_scene (&self) -> i32{
+    fn get_actual_scene(&self) -> i32 {
         return self.actual_scene;
+    }
+    fn get_scene(&self, position: usize) -> &Scene{
+        return &self.scenes[position];
+    }
+    #[allow(warnings)]
+    fn add_scene(&mut self, scene: Scene) { //
+        //scenes.p
     }
 }
 
@@ -25,16 +43,50 @@ struct Scene {
 }
 
 impl Scene {
-    
-
+    fn set_id(&mut self, id: i32) {
+        self.id = id;
+    }
+    fn get_id(&mut self) -> i32 {
+        return self.id;
+    }
+    fn set_title(&mut self, title: String) {
+        self.title = title;
+    }
+    fn get_title(&self) -> &String {
+        return &self.title;
+    }
+    fn set_description(&mut self, description: String) {
+        self.description = description;
+    }
+    fn get_description(&self) -> &String {
+        return &self.description;
+    }
+    fn add_objects() {
+        
+    }
+    fn recover_objects() {
+        
+    }
 }
 
 struct Inventory {
+    id: i32,
     objects: [Object; 100],
 }
 
 impl Inventory {
-
+    fn set_id(&mut self, id: i32) {
+        self.id = id;
+    }
+    fn get_id(&self) -> i32 {
+        return self.id;
+    }
+    fn add_objects() { //função do jogador (get Object)
+        
+    }
+    fn recover_objects() {
+        
+    }
 }
 
 struct Object {
@@ -51,24 +103,64 @@ struct Object {
 }
 
 impl Object {
-    fn pick_object() {
+    fn pick_object(&mut self) { //função comando do jogador
+        self.obtained = true;
+    }
+    fn check_object(&self) { //função comando do jogador
+        println!("\n{}\n", self.description);
+    }
+    fn use_object() { //função comando do jogador
 
+    }
+    fn set_resolved(&mut self, state: bool) {
+        self.resolved = state;
+    }
+    fn get_resolved(&self) -> bool {
+        return self.resolved;
+    }
+    fn set_obtained(&mut self, state: bool) {
+        self.obtained = state;
+    }
+    fn get_obtained(&self) -> bool {
+        return self.obtained;
+    }
+    fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+    fn get_name(&self) -> &String { //error: cannot move out of borrowed content
+        return &self.name;          //solution: return a reference
     }
 }
 
-fn save_game (filename: &str) {
+//let x = Game::new();
+#[allow(warnings)]
+fn list_inventory(inventory: Inventory) { //move to inventory impl
+    //let inventory_objects: i32 = inventory.get_objects();
+    //print
+}
+
+fn save_game(filename: &str) {
     println!("{}", filename);
 }
 
-fn load_saved_game (filename: &str) {
+fn load_saved_game(filename: &str) {
     println!("{}", filename);
 }
 
-fn new_game () {
+fn new_game() {
+    //let mut new_game = Game;
     println!("Reiniciar jogo? (S/N):\n");
 }
 
-fn parse_user_command (command: &str) { //identificar qual comando foi digitado pelo jogador
+fn get_help() {
+
+}
+
+fn use_inv_object_with_scene_object() { //função comando do jogador (mudar estado do objeto cena e inventario)
+
+}
+
+fn parse_user_command(command: &str) { //identificar qual comando foi digitado pelo jogador
     println!("{}", command);
 }
 
@@ -83,5 +175,18 @@ fn main() {
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    //println!("With text:\n{}", contents);
+    let mut n = 1;
+    while n!=3 {
+        let mut input = String::new();
+        println!("/>");
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{} bytes read", n);
+                //println!("{}", input);
+            }
+            Err(error) => println!("error: {}", error),
+        }
+        n = n + 1;
+    }
 }
